461f7303af1936dd519600fb2346cbf5c4bae963
torpedodev <45216006+torpedodev@users.noreply.github.com>
2019-07-14 20:54:57 -0300

    Programacao das armadilhas - Atividade 2 UC07
    Programacao das armadilhas e botao de pulo conforme pedido.

---
+++ AlienCity3D_Fase5_V5.5/Assets/acionador.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acionador : MonoBehaviour
+{
+    public estala1 estala;
+    public estala1 estala2;
+    public estala1 estala3;
+    public estala1 estala4;
+    public estala1 estala5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Debug.Log("armadilha");
+            estala.Fallen();
+        estala2.Fallen();
+        estala3.Fallen();
+        estala4.Fallen();
+        estala5.Fallen();
+        Destroy(gameObject, 2f);
+    }
+        
+}
---
+++ AlienCity3D_Fase5_V5.5/Assets/estala1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class estala1 : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    public void Fallen()
+    {
+        // if(trigger.gameObject.CompareTag("Player"))
+        {
+            rb.useGravity = true;
+
+            Destroy(gameObject, 2f);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
